566959f4325b25a95bcac0285e2d7a651c2c6e87
문정민 <moondeve41@gmail.com>
2023-03-09 23:17:25 +0900

    first commit

---
+++ src/test/kotlin/com/example/springtech/SpringTechApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.springtech
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SpringTechApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
